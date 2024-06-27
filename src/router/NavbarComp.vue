f7313000124a36547c987ad3fd677b94b1b20936
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-27 16:18:37 +0200

    nav

--- src/router/NavbarComp.vue
+++ src/router/NavbarComp.vue
@@ -1,11 +1,11 @@
 <template>   
     <nav class="navbar navbar-expand-lg fixed-top shadow d-flex justify-content-end">
         <div class="navbar-nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/education">Education</router-link> |
-            <router-link to="/projects">Projects</router-link> |
-            <router-link to="/testimonials">Testimonials</router-link> |
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/education">Education</router-link>
+            <router-link to="/projects">Projects</router-link>
+            <router-link to="/testimonials">Testimonials</router-link>
             <router-link to="/contact">Contact</router-link> 
         </div>
     </nav>
@@ -22,8 +22,15 @@ export default {
     }
 
     nav a {
+        padding: 10px;
         font-weight: bold;
+        font-size:20px;
         color:white;
+        text-decoration: none;
+        transition: 1s;
+    }
+    nav a:hover{
+        text-shadow:5px 5px black;
     }
 
     nav a.router-link-exact-active {
