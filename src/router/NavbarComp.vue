bde233400e0879d90f71dcabca361d2192c6b5e3
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-27 15:57:54 +0200

    mmyPortfolio

--- src/router/NavbarComp.vue
+++ src/router/NavbarComp.vue
@@ -1,10 +1,10 @@
-<template lang="">
+<template>   
     <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/">About</router-link>
-        <router-link to="/">Education</router-link>
-        <router-link to="/">Projects</router-link>
-        <router-link to="/">Contact</router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/">Education</router-link> |
+        <router-link to="/">Projects</router-link> |
+        <router-link to="/">Contact</router-link> |
         <router-link to="/">Testimonials</router-link>
     </nav>
 </template>
@@ -14,5 +14,16 @@ export default {
 }
 </script>
 <style scoped>
-    
+    nav {
+    padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: black;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <navbar-comp>
+        <router-view/>
+    </navbar-comp>
+    <div>
+        
+    </div>
+
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import NavbarComp from '@/router/NavbarComp.vue';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    components:{
+        NavbarComp
+    }
 }
 </script>
+<style lang="">
+    
+</style>
