fd73a3ee7c39094a1c1a2a7596d6c8135ba5a5e9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-27 14:40:16 +0200

    myPortfolio

---
+++ src/router/NavbarComp.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/">About</router-link>
+        <router-link to="/">Education</router-link>
+        <router-link to="/">Projects</router-link>
+        <router-link to="/">Contact</router-link>
+        <router-link to="/">Testimonials</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    
+</style>
