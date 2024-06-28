c2add1965cfe0f4eb8e7c07aea914cbf1a2f97d2
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-28 15:44:46 +0200

    home

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12" id="pic">
-               <h2 class="position-relative">Pic</h2> 
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="pic">
+               <h2>Pic</h2> 
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-12" id="text">
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="text">
                 <h2>Text</h2> 
             </div>
         </div>
