e97f7fd2f552bca12aa66d0b86f11c416e219684
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-01 13:33:53 +0200

    home

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="text">
-               <h2>MXM</h2> 
-            </div>
             <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="pic">
-                <h2>Text</h2> 
+               <h2>Text</h2> 
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="text">
+                <img src="https://c0dingforfun.github.io/portfolioimg/Images/Landing/Landing.png"/>
             </div>
         </div>
     </div>
@@ -19,14 +19,20 @@ export default {
     #pic{
         min-height:100vh;
         max-height: auto;
-        background-color:#125a3e;
+        background-color:#ffc695;
         color:aliceblue;
+    }
+    #text img{
+        width:48%;
+        height:auto;
     }
     #text{
         min-height:100vh;
         max-height: auto;
-        background-color: #ffc695;
+        background-color: #125a3e;
         color:aliceblue;
         
     }
+
+
 </style>
