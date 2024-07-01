51676b377708ee3eae186025460a224d735c399e
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-01 09:10:01 +0200

    home

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="pic">
-               <h2>Pic</h2> 
+               <h2>MXM</h2> 
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="text">
                 <h2>Text</h2> 
@@ -19,12 +19,13 @@ export default {
     #pic{
         min-height:100vh;
         max-height: auto;
-        background-color:teal;
-        
+        background-color:#125a3e;
+        color:aliceblue;
     }
     #text{
         min-height:100vh;
         max-height: auto;
-        background-color: orange;
+        background-color: #ffc695;
+        color:aliceblue;
     }
 </style>
