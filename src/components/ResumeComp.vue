45e572318e354719e00e68fec87a921822a06c73
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-02 14:37:08 +0200

    resume

--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -3,13 +3,31 @@
         <div class="row mt-3">
             <div class="col-lg-1 col-md-1"></div>
             <div class="col-lg-5 col-md-5 col-sm-12">
-                <div class="education align-content-center">
-                    <h2>Pluh</h2>
+                <div class="education">
+                    <div class="row">
+                        <div class="col-1 timeline">
+                            <img src="" alt="">
+                        </div>
+                        <div class="col-11">
+                            <div>hdh</div>
+                            <div>hdh</div>
+                            <div>hdh</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-5 col-md-5 col-sm-12">
-                <div class="experience align-content-center">
-                    <h2>Pluh</h2>
+                <div class="experience">
+                    <div class="row">
+                        <div class="col-1">
+                            |
+                        </div>
+                        <div class="col-11">
+                            <div>hdh</div>
+                            <div>hdh</div>
+                            <div>hdh</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-1 col-md-1"></div>
@@ -52,5 +70,9 @@ export default {
         background-color: #125a3e;
         color:aliceblue;
         margin-left:5px;
+    }
+    .timeline img{
+        width:auto;
+        height:100%;
     }
 </style>
