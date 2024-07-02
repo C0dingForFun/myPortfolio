a465fbcebd99da5648b978f5367a8de85da765f5
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-02 12:06:13 +0200

    resume

---
+++ src/components/ResumeComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid" id="resume">
+        <div div="row">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    #resume{
+        min-height:100vh;
+        max-height: auto;
+        background-color: #ffc695;
+        color:aliceblue;
+    }
+</style>
