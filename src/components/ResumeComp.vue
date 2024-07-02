a16187575c2a38a8d1cb80fefd35862679f72ed9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-02 12:36:36 +0200

    resume

--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -1,26 +1,51 @@
 <template>
-    <div class="container-fluid align-content-center justify-content-center" id="resume">
-        <div div="row align-content-center" id="row">
-            <h2>Resume</h2>
+    <div class="container-fluid" id="resume">
+        <div div="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="green">
+               <h2>hi</h2> 
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="peach">
+               <h2>hello</h2> 
+            </div>
+            <!-- <div class="col-lg-3 col-md-3 col-sm-12 align-content-center" id="green">hola</div>
+            <div class="col-lg-3 col-md-3 col-sm-12 align-content-center" id="peach">molo</div> -->
+            <!-- <h2>Resume</h2> -->
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    // methods:{
+    //     displayAboutMe(){
+    //         return this.$store.state.aboutMe
+    //     }
+    // },
+    // computed:{
+    //     getData(){
+    //        return this.$store.dispatch('getData')
+    //     }
+    // },
+    // mounted(){
+    //     this.getData
+    // }
 }
 </script>
 <style>
-    #resume{
+    /* #resume{
         min-height:100vh;
         max-height: auto;
-        background-color: #ffc695;
+        /* background-color: #ffc695; 
         color:aliceblue;
-    }
-    #row{
-        width:95vw;
-        height:95vh;
+    } */
+    #green{
+        min-height:100vh;
+        max-height: auto;
         background-color:#125a3e;
+    }
+    #peach{
+        min-height:100vh;
+        max-height: auto;
+        background-color:#ffc695;
     }
     h2{
         color:aliceblue;
