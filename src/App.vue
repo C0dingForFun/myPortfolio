3185c33281f1390461f948378ba37a6f6648006b
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-03 14:03:51 +0200

    main

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
 body{
   overflow-x:hidden;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Comic Neue", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
