fbaf0add0468d954c6141e211cbce8ae6bedf817
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-03 08:33:57 +0200

    about

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid" id="about" v-for="myDescription in displayAboutMe()" :key="myDescription">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 align-content-center" id="pic">
-                <img src="https://c0dingforfun.github.io/portfolioimg/Images/Me/Me.png" class="rounded"/>
+                <img :src='myDescription.image' class="rounded"/>
             </div>
-            <div v-for="myDescription in displayAboutMe()" :key="myDescription" class="col-lg-6 col-md-6 col-sm-12 align-content-center text-white" id="text">
-                <div class="text-start mx-auto" style="width:300px;">
+            <div class="col-lg-6 col-md-6 col-sm-12 align-content-center text-white" id="text">
+                <div class="text-start mx-auto" style="width:420px;">
                     <h5>Discover</h5>
                     <h2><b>About Me</b></h2>
                     <p>{{myDescription.me}}</p> 
@@ -18,10 +18,9 @@
                             <p>Age: {{ myDescription.age }}</p>
                             <p>Aspiring: {{ myDescription.aspiringToBe }}</p>
                         </div>
+                        <a href="file.doc" class="mx-auto mb-1 align-content-center text-center" download>Download CV</a>
                     </div> 
                 </div>
-                
-                
             </div>
             
         </div>
@@ -66,7 +65,21 @@ export default {
         color:#125a3e; 
     }
     .extra-info{
-        width:400px;
-        border:solid 2px #125a3e;
+        width:410px;
+        background-color:#125a3e;
+        box-shadow:10px 10px 10px rgb(0,0,0,0.5); 
+    }
+    a{
+        width:200px;
+        height:40px;
+        background-color: #ffc695;
+        color:aliceblue;
+        transition: 1s;
+        text-decoration: none;
+    }
+    a:hover{
+        transform: scale(1.05);
+        box-shadow: 8px 8px 8px rgb(0,0,0,0.5);
+        background-color: black;
     }
 </style>
