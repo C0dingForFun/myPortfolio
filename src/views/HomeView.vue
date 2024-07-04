01f868f3a57ad3b5c6403e5e981916cf1d7528ed
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-04 14:08:17 +0200

    skills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,53 +10,59 @@
                     <div id="skills" v-for="skill in displaySkills()" :key="skill">
                         <h2 class="text-center pt-5" data-aos="fade-up"> Skills </h2>
                         <div class="row d-flex justify-content-center py-3 m-3">
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
                                     <i :class="skill.programmingSkills[0].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[0].level }}
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
                                     <i :class="skill.programmingSkills[1].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[1].level }}
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
                                     <i :class="skill.programmingSkills[2].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[2].level }}
                             </div>
                         </div>
                         <div class="row d-flex justify-content-center">
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.programmingSkills[3].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[3].level }}
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.programmingSkills[4].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[4].level }}
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.programmingSkills[5].iconClass"></i>
                                 </div>
+                                {{ skill.programmingSkills[5].level }}
                             </div>
                         </div>
                         <div class="row d-flex justify-content-center">
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.softSkills[0].iconClass"></i>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.softSkills[1].iconClass"></i>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills align-content-center">
                                 <div>
-                                    <!-- <i :class="skill.programmingSkills[0].iconClass"></i> -->
+                                    <i :class="skill.softSkills[2].iconClass"></i>
                                 </div>
                             </div>
                         </div>
@@ -101,10 +107,11 @@ export default {
         min-height:100vh;
         max-height:auto;
         background-color: #125a3e;
+        
     }
     .skills{
         width:210px;
-        height:210px;
+        /* height:210px; */
         font-size:18px;
         color:aliceblue;
         background-color: #ffc695;
