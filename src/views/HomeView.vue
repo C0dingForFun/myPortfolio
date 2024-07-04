73879191129d616ec27cf76a4886469e24dbca4a
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-04 11:47:05 +0200

    skills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,33 +10,53 @@
                     <div id="skills" v-for="skill in displaySkills()" :key="skill">
                         <h2 class="text-center pt-5" data-aos="fade-up"> Skills </h2>
                         <div class="row">
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-                                
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-                                
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-                                
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-                                
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12">
-                                
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
                             </div>
-                            <div class="col-lg-4 col-col-md-4 col-sm-12"></div>
+                            <div class="col-lg-4 col-col-md-4 col-sm-12 skills">
+                                <div>
+                                    <i :class="skill.iconClass"></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </template> 
@@ -95,121 +115,8 @@ export default {
     .skills i{
         font-size:50px;
     }
-
-    #html:hover{
-        box-shadow: 2px 3px red;
-        text-shadow:2px 2px red ;
-        transition: 1s;
-    }
-
-    #css:hover{
-        box-shadow: 2px 3px dodgerblue;
-        text-shadow:2px 2px dodgerblue;
-        transition: 1s;
-    }
-
-    #git:hover{
-        box-shadow: 2px 3px orangered;
-        text-shadow:2px 2px orangered;
-        transition: 1s;
-    }
-
-    #bootstrap:hover{
-        box-shadow: 2px 3px purple;
-        text-shadow:2px 2px purple;
-        transition: 1s;
-    }
-
-    #team:hover{
-        box-shadow: 2px 3px blue;
-        text-shadow:2px 2px blue;
-        transition: 1s;
-    }
-
-    #ears:hover{
-        box-shadow: 2px 3px yellowgreen;
-        text-shadow:2px 2px yellowgreen;
-        transition: 1s;
-    }
-
-    #mobile:hover{
-        box-shadow: 2px 3px brown;
-        text-shadow:2px 2px brown;
-        transition: 1s;
-    }
-
-    #microphone:hover{
-        box-shadow: 2px 3px #696969;
-        text-shadow:2px 2px #696969;
-        transition: 1s;
-    }
-
     .skills:hover{
         cursor:pointer;
         transition: 1s;
-    }
-
-    .skills:hover > .la-html5{
-        cursor: pointer;
-        color:red;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px red;
-    }
-
-    .skills:hover > .la-css3-alt{
-        cursor: pointer;
-        color:dodgerblue;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px dodgerblue;
-    }
-
-    .skills:hover > .la-git{
-        cursor: pointer;
-        color:orangered;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px orangered;
-    }
-
-    .skills:hover > .la-bootstrap{
-        cursor: pointer;
-        color:purple;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px purple;
-    }
-
-    .skills:hover > .la-users{
-        cursor: pointer;
-        color:blue;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px blue;
-    }
-
-    .skills:hover > .la-assistive-listening-systems{
-        cursor: pointer;
-        color:yellowgreen;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px yellowgreen;
-    }
-
-    .skills:hover > .la-mobile{
-        cursor: pointer;
-        color:brown;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px brown;
-    }
-
-    .skills:hover > .la-microphone{
-        cursor: pointer;
-        color:#696969;
-        background-color: white;
-        border-radius:50%;
-        box-shadow: 2px 3px #696969;
     }
 </style>
