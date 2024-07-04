04691cb3d8adcdf511ae2e5626dcb0033345465e
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-07-04 11:38:03 +0200

    skills

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,53 +6,11 @@
         <about-comp/>
         <resume-comp/>
         <skills-comp>
-                <template #skills>
-                    <div id="skills">
+                <template #skills >
+                    <div id="skills" v-for="skill in displaySkills()" :key="skill">
                         <h2 class="text-center pt-5" data-aos="fade-up"> Skills </h2>
-                        <div class="row justify-content-evenly mt-5">
-                          <div id="html" class="col-lg-3 col-md-6 col-sm-12 skills align-content-center align-content-center" data-aos="zoom-in">
-                            <i class="lab la-html5"></i>
-                            <h3>HTML5</h3>
-                            <p>Advanced</p>
-                          </div>
-                          <div id="css" class="col-lg-4 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="500">
-                            <i class="lab la-css3-alt"></i>
-                            <h3>CSS3</h3>
-                            <p>Advanced</p>
-                          </div>
-                          <div id="git" class="col-lg-4 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="1000"> 
-                            <i class="lab la-git"></i>
-                            <h3>Git</h3>
-                            <p>Advanced</p>
-                          </div>
-                          <div id="bootstrap" class="col-lg-6 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="1500">
-                            <i class="lab la-bootstrap"></i>
-                            <h3>Bootstrap5</h3>
-                            <p>Intermediate</p>
-                          </div>
-        
-                          <div class="row justify-content-evenly mt-5 mt-3">
-                            <div id="team"  class="col-lg-4 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="500">                  
-                              <i class="las la-users"></i>
-                              <h3>Team Player</h3>
-                             <p>I love working in groups</p>
-                            </div>
-                            <div id="ears" class="col-lg-4 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="1000">                  
-                              <i class="las la-assistive-listening-systems"></i>
-                              <h3>Good Listener</h3>
-                              <p>I enjoy listening to others.</p>
-                            </div>
-                            <div id="mobile" class="col-lg-4 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="1500">                  
-                              <i class="las la-mobile"></i>
-                              <h3>Responsive</h3>
-                              <p>Advanced at creating responsiveness</p>
-                            </div>
-                            <div id="microphone" class="col-lg-6 col-md-6 col-sm-12 skills align-content-center" data-aos="zoom-in" data-aos-delay="4000">                  
-                              <i class="las la-microphone"></i>
-                              <h3>Communicate</h3>
-                              <p>I am not afraid to voice my opinions.</p>
-                            </div>
-                          </div>
+                        <div>
+                            
                         </div>
                     </div>
                 </template> 
@@ -71,6 +29,19 @@ export default {
         AboutComp,
         ResumeComp,
         SkillsComp
+    },
+    methods:{
+        displaySkills(){
+            return this.$store.state.skills
+        }
+    },
+    computed:{
+        getData(){
+           return this.$store.dispatch('getData')
+        }
+    },
+    mounted(){
+        this.getData
     }
 }
 </script>
