14055aa1ed666f91330e59dfe58a81dda20b8a57
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-09-16 14:06:03 +0200

    portfolio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,12 @@
         </navbar-comp>
         <home-comp/>
         <about-comp/>
+        <resume-comp/>
             <skills-comp>
                     <template #skills >
                         <div id="skills" v-for="skill in displaySkills()" :key="skill">
                             <h2 class="text-center pt-5" data-aos="fade-up"> Skills </h2>
-                            <div class="row d-flex justify-content-center py-3 mt-5">
+                            <div class="row d-flex justify-content-center py-3">
                                 <div class="col-lg-4 col-col-md-4 col-sm-12 skills pb-2 align-content-center">
                                     <div>
                                         <i :class="skill.programmingSkills[0].iconClass"></i>
@@ -123,7 +124,7 @@
                     <div class="row projects" >
                         <h2>Projects</h2>
                         <div class="row">
-                            <div class="card col-lg-5 col-md- col-sm-12" v-for="project in displayProjects()" :key="project" id="projects">
+                            <div class=" card col-lg-5 col-md- col-sm-12" style="width: 20rem;" v-for="project in displayProjects()" :key="project" id="projects">
                                 <img :src="project.image" class="card-img-top" loading="lazy">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ project.name }}</h5>
@@ -136,7 +137,46 @@
                     </div>
                 </div>
             </template>
-        </projects-comp> 
+        </projects-comp>   
+        <testimonials-comp>
+            <template #b1>
+                <button id="position" class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon " aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+            </template>
+            <template #testimonials>
+                <div class="container-fluid align-items-center"  id="testimonials">
+                    <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+                        <h2 class="text">Testimonials</h2>
+                        <div class="carousel-inner">
+                            <div class="carousel-item testimonials" v-for="(testimonial,index) in displayTestimonials()" :key="testimonial" :class="{'active':index==0}">
+                                <div class="row w-100">
+                                    <div class="col-2">
+                                        <img :src="testimonial.image" class="d-block bg-white">
+                                    </div>
+                                    <div class="col-10 pt-4">
+                                        <span>
+                                            <b>{{ testimonial.name }}<br>
+                                            {{ testimonial.position }}</b>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="pt-3">
+                                    {{ testimonial.message }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <template #b2>
+                <button id="position" class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </template>
+        </testimonials-comp>
         <contact-comp/>
         <footer-comp/>
 </template>
@@ -144,8 +184,10 @@
 import NavbarComp from '@/components/NavbarComp.vue';
 import HomeComp from '@/components/HomeComp.vue';
 import AboutComp from '@/components/AboutComp.vue';
+import ResumeComp from '@/components/ResumeComp.vue';
 import SkillsComp from '@/components/SkillsComp.vue';
 import ProjectsComp from '@/components/ProjectsComp.vue';
+import TestimonialsComp from '@/components/TestimonialsComp.vue';
 import ContactComp from '@/components/ContactComp.vue';
 import FooterComp from '@/components/FooterComp.vue';
 export default {
@@ -153,8 +195,10 @@ components:{
     NavbarComp,
     HomeComp,
     AboutComp,
+    ResumeComp,
     SkillsComp,
     ProjectsComp,
+    TestimonialsComp,
     ContactComp,
     FooterComp
 },
@@ -164,6 +208,9 @@ components:{
         },
         displayProjects(){
             return this.$store.state.projects
+        },
+        displayTestimonials(){
+            return this.$store.state.testimonials
         },
 },
 computed:{
@@ -208,17 +255,32 @@ mounted(){
     .skills i{
         font-size:5vw;
     }
+    #projects > .card{
+        background-color: #ffc695;
+    }
     #projects{
-        width:100vw;
-        height:100vh;
         margin:auto;
         background-color: #125a3e;
         color:aliceblue;
     }
-    #projects .card{
-        width:18rem;
-        background-color: #ffc695;
-        height:10vh;
+    #testimonials{
+        min-height:80vh;
+        max-height:auto;
+        background-color: #229668;
+    }
+    span{
+        margin-right: 20px;
+    }
+    .testimonials{
+        height:33vh;
+        background-color:#125a3e;
+        color:aliceblue;
+        border-radius:15px;
+    }
+    .testimonials img{
+        width: 100px;
+        height:100px;
+        border-radius:50%;
     }
 
 @media only screen and (max-width:600px){
